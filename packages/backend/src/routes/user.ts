d7655c10cf3d65117965b847b1cbbbf658f7b25f
Marcos Capistrano <mcapistrano50@gmail.com>
2021-11-19 11:25:45 -0300

    adicionado radius no pivot-map

--- packages/backend/src/routes/user.ts
+++ packages/backend/src/routes/user.ts
@@ -5,43 +5,60 @@ import {
   deleteUserController
 } from '../controllers/user';
 import { DuplicateUniqueError } from '../types/errors';
+import db from '../database';
 
 const router = express.Router();
 
-  router.post('/signup', async (req, res, next) => {
-    const { login, password, user_type } = req.body;
+router.post('/signup', async (req, res, next) => {
+  const { login, password, user_type } = req.body;
 
-    try {
-      const cookieInfo = await signUpController(login, password, user_type);
+  try {
+    const cookieInfo = await signUpController(login, password, user_type);
 
-      res.send(cookieInfo);
-    } catch (err) {
-      next(err);
+    res.send(cookieInfo);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.post('/signin', async (req, res, next) => {
+  const { login, password } = req.body;
+
+  try {
+    const cookieInfo = await signInController(login, password);
+
+    res.send(cookieInfo);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/auth/:user_id', async (req, res, next) => {
+  const { user_id } = req.params;
+
+  try {
+    const user = await db.user.findFirst({ where: { user_id } });
+
+    if (user) {
+      res.send({ msg: 'OK' });
+    } else {
+      res.status(401).send({ msg: 'Unauthorized' });
     }
-  });
+  } catch (err) {
+    next(err);
+  }
+});
 
-  router.post('/signin', async (req, res, next) => {
-    const { login, password } = req.body;
+router.delete('/:user_id', async (req, res, next) => {
+  const user_id = req.params.user_id;
 
-    try {
-      const cookieInfo = await signInController(login, password);
+  try {
+    const response = await deleteUserController(user_id);
 
-      res.send(cookieInfo);
-    } catch (err) {
-      next(err);
-    }
-  });
+    res.send(response);
+  } catch (err) {
+    next(err);
+  }
+});
 
-  router.delete('/:user_id', async (req, res, next) => {
-    const user_id = req.params.user_id;
-
-    try {
-      const response = await deleteUserController(user_id);
-
-      res.send(response);
-    } catch (err) {
-      next(err);
-    }
-  });
-
-  export default router;
+export default router;
