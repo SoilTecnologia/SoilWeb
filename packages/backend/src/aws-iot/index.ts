e28da5841b1f44cd7a932788f25353745f3ba438
Marcos Capistrano <mcapistrano50@gmail.com>
2021-11-27 13:50:34 -0300

    Adicionado sistema de mensagens pendentes

--- packages/backend/src/aws-iot/index.ts
+++ packages/backend/src/aws-iot/index.ts
@@ -18,6 +18,8 @@ class IoTDevice {
   private subTopic: string = '';
   private clientId: string = '';
   private connection: mqtt.MqttClientConnection;
+
+  private pendingMessages: Array<any> = [];
   // endpoint
 
   constructor(type: IoTDeviceType, qos: 0 | 1, topic?: string) {
@@ -38,7 +40,7 @@ class IoTDevice {
     const keyPath = './src/aws-iot/private.pem.key';
     const endpoint = 'a19mijesri84u2-ats.iot.us-east-1.amazonaws.com';
 
-    console.log("Starting Cloud.. Wait for connection..")
+    console.log('Starting Cloud.. Wait for connection..');
 
     try {
       let configBuilder: iot.AwsIotMqttConnectionConfigBuilder;
@@ -63,37 +65,51 @@ class IoTDevice {
         this.qos,
         this.processMessage
       );
-      console.log("Connected!")
+      console.log('Connected!');
     } catch (err) {
       console.log(err);
-      await this.start(); 
+      await this.start();
     }
 
     if (this.type == 'Cloud') {
-      console.log("CLOUD")
+      console.log('CLOUD');
       emitter.on('intent', async (intentDetails) => {
-        console.log("OPAA")
-        console.log(intentDetails)
+        console.log('OPAA');
+        console.log(intentDetails);
         const { power, water, direction, percentimeter } = intentDetails.intent;
         const { pivot_id, node_name, farm_name } = intentDetails;
 
         // Publish intent updates to nodes
 
-        console.log("GOT HERE")
-        if (!pivot_id) {
-          console.log(`Publishing down to a GPRS: ${farm_name}/${node_name}`);
-          await this.publish(
-            IntentToString(power, water, direction, percentimeter),
-            `${farm_name}/${node_name}`
-          );
-        } else {
-          // console.log(`Publishing down to a Raspberry: ${farm_name}/${node_name}`);
-          // await this.publish(
-          //   { pivot_id, power, water, direction, percentimeter },
-          //   `${farm_name}/${node_name}`
-          // );
-        }
+        console.log('Adding new Intent to pending messages...');
+        this.pendingMessages.push(intentDetails);
       });
+    }
+
+    setInterval(() => {
+      this.checkPendingMessages();
+    }, 20000);
+  }
+
+  async checkPendingMessages() {
+    for (let pendingMessage of this.pendingMessages) {
+      const { power, water, direction, percentimeter } = pendingMessage.intent;
+      const { pivot_id, node_name, farm_name } = pendingMessage;
+      if (!pivot_id) {
+        console.log(`Publishing down to a GPRS: ${farm_name}/${node_name}`);
+        await this.publish(
+          IntentToString(power, water, direction, percentimeter),
+          `${farm_name}/${node_name}`
+        );
+      } else {
+        console.log(
+          `Publishing down to a Raspberry: ${farm_name}/${node_name}`
+        );
+        await this.publish(
+          JSON.stringify({ pivot_id, power, water, direction, percentimeter }),
+          `${farm_name}/${node_name}`
+        );
+      }
     }
   }
 
@@ -105,7 +121,7 @@ class IoTDevice {
     console.log('publishing...');
     try {
       await this.connection.publish(finalTopic!, payload, 1, false);
-      console.log("Published!")
+      console.log('Published!');
     } catch (err) {
       console.log(
         `Error publishing to topic: ${finalTopic} from ${this.clientId}`,
@@ -122,40 +138,41 @@ class IoTDevice {
     retain: boolean
   ) => {
     const decoder = new TextDecoder('utf8');
-    // console.log(payload)
-
-    // if (this.type == 'Cloud') {
-    //   const json: PivotToCloudMessage = JSON.parse(decoder.decode(payload));
-    //   const{type, pivot_name, node_id, power, direction, water, angle, percentimeter, connection, timestamp} = json;
-
-    //   if(type == "status") {
-    //   // updatePivotController(pivot_name, "ONLINE", node_id, power, water, direction, angle, percentimeter);
-    //   }
-    // }
-
-    // console.log(
-    //   `Publish received. topic:"${topic}" dup:${dup} qos:${qos} retain:${retain}`
-    // );
-    console.log('RECEBIDOOOO');
     try {
-    console.log(JSON.parse(decoder.decode(payload)));
-    }catch(err) {
-      console.log("Failed to parse message:");
-      console.log(payload)
+      const json = JSON.parse(decoder.decode(payload));
+
+      if (this.type == 'Cloud') {
+        const { node_name, farm_name } = json;
+        const { pivot_id, payload } = json;
+
+        console.log(`Received message from ${farm_name}/${node_name}`);
+        console.log(pivot_id)
+        if (!pivot_id) {
+          for (let pendingMessage of this.pendingMessages) {
+            console.log(pendingMessage);
+            if (
+              pendingMessage.node_name === node_name &&
+              pendingMessage.farm_name === farm_name
+            ) {
+              console.log("Removing pending message: ", pendingMessage);
+              this.pendingMessages = this.pendingMessages.filter(
+                ({ node_name, farm_name }) =>
+                  node_name != pendingMessage.node_name &&
+                  farm_name != pendingMessage.farm_name
+              );
+            }
+          }
+        }
+      }
+    } catch (err) {
+      console.log('Failed to decode message: ');
+      console.log(err);
     }
 
-    if (this.type == 'Cloud') {
-      // const ESPPayload: ESPToCloudMessage = JSON.parse(decoder.decode(payload as any));
-      // const {type, node_id, pivot_name, esp_payload} = ESPPayload;
-      // const {power, connection, water, direction, angle, percentimeter, timestamp} = StatusStringToPrisma(esp_payload)
-      // await updatePivotController(pivot_name, connection, node_id, power, water, direction, angle, percentimeter, /*timestamp*/)
-    } else if (this.type == 'Raspberry') {
-      //   const ESPPayload = JSON.parse(decoder.decode(payload as any));
-      //   const {type, node_id, pivot_name, esp_payload} = ESPPayload;
-      //   const {power, connection, water, direction, angle, percentimeter, timestamp} = StatusStringToPrisma(esp_payload)
-      //   await updatePivotController(pivot_name, connection, node_id, power, water, direction, angle, percentimeter, /*timestamp*/)
-      // }
-    }
+    console.log(
+      `Publish received. topic:"${topic}" dup:${dup} qos:${qos} retain:${retain}`
+    );
+    console.log('RECEBIDOOOO');
   };
 }
 
