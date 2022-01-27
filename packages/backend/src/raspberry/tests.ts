6477af308d3e4318ff82302501d2fc54763d0a78
Marcos Capistrano <mcapistrano50@gmail.com>
2022-01-27 16:24:35 -0300

    aumentado o limite de fails

--- packages/backend/src/raspberry/tests.ts
+++ packages/backend/src/raspberry/tests.ts
@@ -136,8 +136,6 @@ const checkPool = async () => {
       );
 
       if (payloadObject && current.action.radio_id == data.id && checkResponse(current.action, payloadObject)) {
-        console.log("Updating PIVOT")
-        console.log(payloadObject);
         await updatePivotController(
           current.action.pivot_id,
           true,
@@ -156,7 +154,6 @@ const checkPool = async () => {
         await updateActionController(current.action.action_id, true);
         activeQueue.dequeue();
       } else {
-        console.log("NO PAYLOAD OR WRONG RESPONSE ID");
         current.attempts++;
       }
     } catch (err) {
@@ -197,8 +194,6 @@ const checkPool = async () => {
       );
 
       if (payloadObject && current.radio_id == data.id) {
-        console.log("Updating PIVOT")
-        console.log(payloadObject);
         await updatePivotController(
           current.pivot_id,
           true,
@@ -213,7 +208,6 @@ const checkPool = async () => {
         );
         current.attempts = 0;
       } else {
-        console.log("NO PAYLOAD OR WRONG RESPONSE ID");
         current.attempts++;
       }
     } catch (err) {
