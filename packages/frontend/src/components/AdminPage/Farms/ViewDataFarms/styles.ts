09f4b1bb714c426ede3dcd2cc45b8cb477a4b07d
Henrique <henrique.multitech@gmail.com>
2022-03-15 18:07:12 -0300

    iniciando na branch dev

--- packages/frontend/src/components/AdminPage/Farms/ViewDataFarms/styles.ts
+++ packages/frontend/src/components/AdminPage/Farms/ViewDataFarms/styles.ts
@@ -13,6 +13,12 @@ export const Container = styled.div`
     justify-content: center;
   `}
 `;
+export const ContentFullData = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+`;
 export const ContentData = styled.div`
   display: flex;
   flex-direction: column;
