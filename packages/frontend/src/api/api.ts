6ae66cd8734fcd98d727f5c4ebe8e05117f2cd83
Marcos Capistrano <mcapistrano50@gmail.com>
2022-03-16 14:46:34 -0300

    https

--- packages/frontend/src/api/api.ts
+++ packages/frontend/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `https://teste.soiltech.com.br:3308/`,
+  baseURL: `http://localhost:3308/`,
 });
 
 // api.interceptors.request.use((config) => {
