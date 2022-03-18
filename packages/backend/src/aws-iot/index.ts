b6190d0b5163a6e6c23954087e1bc7bf3547bd36
Marcos Capistrano <mcapistrano50@gmail.com>
2022-03-18 16:03:41 -0300

    oi

--- packages/backend/src/aws-iot/index.ts
+++ packages/backend/src/aws-iot/index.ts
@@ -216,8 +216,8 @@ class IoTDevice {
         }
       } else if (json.type === 'action') {
         console.log('[EC2-IOT-ACTION-ACK] Resposta de action recebida');
-        this.queue.remove(json);
-        console.log(this.queue)
+        let removed = this.queue.remove(json);
+        console.log(removed)
       }
     } else if (this.type === 'Raspberry') {
       if (json.type === 'status') {
