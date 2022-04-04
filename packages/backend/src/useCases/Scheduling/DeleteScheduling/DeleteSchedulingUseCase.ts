010f33d48355b6df2a346c2c4cbba532f9435406
Henrique <henrique.multitech@gmail.com>
2022-04-04 11:20:38 -0300

    TRatamento de erro adiciona em scheduling

--- packages/backend/src/useCases/Scheduling/DeleteScheduling/DeleteSchedulingUseCase.ts
+++ packages/backend/src/useCases/Scheduling/DeleteScheduling/DeleteSchedulingUseCase.ts
@@ -1,25 +1,47 @@
 import { inject, injectable } from 'tsyringe';
 import { SchedulingModel } from '../../../database/model/Scheduling';
 import { ISchedulingRepository } from '../../../database/repositories/Scheduling/ISchedulingRepository';
+import { messageErrorTryAction } from '../../../utils/types';
 
 @injectable()
-class DeleteSchedulingUseCase{
-    constructor(
-        @inject('SchedulingRepository') private schedulingRepository : ISchedulingRepository
-    ) {}
+class DeleteSchedulingUseCase {
+  constructor(
+    @inject('SchedulingRepository')
+    private schedulingRepository: ISchedulingRepository
+  ) {}
 
-
-async execute (scheduling_id: SchedulingModel['scheduling_id']){
-    const scheduling = await this.schedulingRepository.findById(scheduling_id);
-    
-    if (scheduling){
-        const scheduling = await this.schedulingRepository.delete(scheduling_id);
-
-        return scheduling;
+  private async applyQueryFindById(scheduling_id: string) {
+    try {
+      return await this.schedulingRepository.findById(scheduling_id);
+    } catch (err) {
+      messageErrorTryAction(
+        err,
+        true,
+        DeleteSchedulingUseCase.name,
+        'Get Scheduling By Id'
+      );
     }
+  }
 
-    throw new Error('Scheduling does not exist')
-}
+  private async applyQueryDelete(scheduling_id: string) {
+    try {
+      return await this.schedulingRepository.delete(scheduling_id);
+    } catch (err) {
+      messageErrorTryAction(
+        err,
+        true,
+        DeleteSchedulingUseCase.name,
+        'Delete Scheduling '
+      );
+    }
+  }
+
+  async execute(scheduling_id: SchedulingModel['scheduling_id']) {
+    const scheduling = await this.applyQueryFindById(scheduling_id);
+
+    if (!scheduling) throw new Error('Scheduling does not exist');
+    return await this.applyQueryDelete(scheduling_id);
+  }
 }
 
-export { DeleteSchedulingUseCase }
+export { DeleteSchedulingUseCase };
