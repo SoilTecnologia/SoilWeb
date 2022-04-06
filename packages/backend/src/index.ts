0f6a8d08f7271e27965e114d0789a80eebfe4be1
Henrique <henrique.multitech@gmail.com>
2022-04-06 17:33:36 -0300

    mudando para raspberry

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -12,7 +12,7 @@ import { createServer } from 'http';
 import 'reflect-metadata';
 import { Server, Socket } from 'socket.io';
 import IoTDevice from './aws-iot';
-// import * as raspberry from './raspberry/tests';
+import * as raspberry from './raspberry/tests';
 import router from './routes';
 import './shared/container';
 import emitter from './utils/eventBus';
@@ -103,10 +103,10 @@ try {
   console.log(err.message);
 }
 
-// raspberry.start();
+raspberry.start();
+const iotDevice = new IoTDevice('Raspberry', 0, 'IGM_1/0');
+// const iotDevice = new IoTDevice('Cloud', 0);
 
-const iotDevice = new IoTDevice('Cloud', 0);
-// const iotDevice = new IoTDevice('Raspberry', 0, 'IGM_1/0');
 iotDevice.start();
 // e5ce95e1-277d-40a7-b843-6d2cb51d1e8f
 // cae38681-5734-4629-b564-31764fef9b97/1
