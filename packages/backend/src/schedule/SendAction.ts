0190f5448245a0cb12427411855809df8bfd9528
IgorMendesPereira <igor_mendes_pereira@outlook.com>
2022-04-20 10:04:30 -0300

    iniciando mapeamento de horários para agendamento

--- packages/backend/src/schedule/SendAction.ts
+++ packages/backend/src/schedule/SendAction.ts
@@ -1,11 +1,18 @@
 const cron = require('node-cron')
+import express from 'express';
 
-
+var app = express();
+const api = 'http://localhost:3308/'
 
 
 function SendAction(){
   console.log("Enviar action quando necessário")
 }
+
+app.get('/scheduling/getAllScheduling',(req, res)=>{
+  
+
+})
 
 module.exports = cron.schedule('*/1 * * * *', SendAction, {
   scheduled: false
