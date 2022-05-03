4068e3824a9c37cda9ee39f9d25a67f28235a26d
IgorMendesPereira <igor_mendes_pereira@outlook.com>
2022-05-03 18:39:17 -0300

    Update pump INCOMPLETE!!!

--- packages/backend/src/routes/pump.ts
+++ packages/backend/src/routes/pump.ts
@@ -3,6 +3,7 @@ import authMiddleware from '../middlewares/auth';
 import { CreatePumpController } from '../useCases/Pumps/CreatePump/CreatePumpController';
 import { DeletePumpController } from '../useCases/Pumps/DeletePump/DeletePumpController';
 import { GetPumpController } from '../useCases/Pumps/GetPump/GetPumpController';
+import { UpdatePumpController } from '../useCases/Pumps/UpdatePump/UpdatePumpController';
 
 
 const router = express.Router();
@@ -10,9 +11,11 @@ const router = express.Router();
 const createPumpController = new CreatePumpController();
 const deletePumpController = new DeletePumpController();
 const getPumpController = new GetPumpController();
+const updatePumpController = new UpdatePumpController();
 
 router.post('/addPump', authMiddleware(), createPumpController.handle);
 router.delete('/deletePump/:id', authMiddleware(), deletePumpController.handle);
 router.get('/getPump/:id', authMiddleware(),getPumpController.handle)
+router.put('/updatePump',authMiddleware(),updatePumpController.handle)
 
 export default router;
