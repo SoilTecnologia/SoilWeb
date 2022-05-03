6989a19bee95e7163628351d5168b7a7fc5de8e5
IgorMendesPereira <igor_mendes_pereira@outlook.com>
2022-05-03 18:19:15 -0300

    Get pump by pivo_id

--- packages/backend/src/routes/pump.ts
+++ packages/backend/src/routes/pump.ts
@@ -2,14 +2,17 @@ import express from 'express';
 import authMiddleware from '../middlewares/auth';
 import { CreatePumpController } from '../useCases/Pumps/CreatePump/CreatePumpController';
 import { DeletePumpController } from '../useCases/Pumps/DeletePump/DeletePumpController';
+import { GetPumpController } from '../useCases/Pumps/GetPump/GetPumpController';
 
 
 const router = express.Router();
 
 const createPumpController = new CreatePumpController();
-const deletePumpController = new DeletePumpController()
+const deletePumpController = new DeletePumpController();
+const getPumpController = new GetPumpController();
 
 router.post('/addPump', authMiddleware(), createPumpController.handle);
 router.delete('/deletePump/:id', authMiddleware(), deletePumpController.handle);
+router.get('/getPump/:id', authMiddleware(),getPumpController.handle)
 
 export default router;
