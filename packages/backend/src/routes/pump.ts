4d5ca824256e3ba1fb9376bd0d2fcfa713066631
IgorMendesPereira <igor_mendes_pereira@outlook.com>
2022-05-04 14:26:45 -0300

    Get All pumps

--- packages/backend/src/routes/pump.ts
+++ packages/backend/src/routes/pump.ts
@@ -4,6 +4,7 @@ import { CreatePumpController } from '../useCases/Pumps/CreatePump/CreatePumpCon
 import { DeletePumpController } from '../useCases/Pumps/DeletePump/DeletePumpController';
 import { GetPumpController } from '../useCases/Pumps/GetPump/GetPumpController';
 import { UpdatePumpController } from '../useCases/Pumps/UpdatePump/UpdatePumpController';
+import { GetAllPumpController } from '../useCases/Pumps/GetAllPump/GetAllPumpController';
 
 
 const router = express.Router();
@@ -12,10 +13,12 @@ const createPumpController = new CreatePumpController();
 const deletePumpController = new DeletePumpController();
 const getPumpController = new GetPumpController();
 const updatePumpController = new UpdatePumpController();
+const getAllPumpController = new GetAllPumpController();
 
 router.post('/addPump', authMiddleware(), createPumpController.handle);
 router.delete('/deletePump/:id', authMiddleware(), deletePumpController.handle);
 router.get('/getPump/:id', authMiddleware(),getPumpController.handle)
 router.put('/updatePump',authMiddleware(),updatePumpController.handle)
+router.get('/getAllPump',authMiddleware(),getAllPumpController.handle)
 
 export default router;
---
+++ packages/backend/src/useCases/Pumps/GetAllPump/GetAllPumpController.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import { container } from "tsyringe";
+import { messageErrorTryAction } from "../../../utils/types";
+import { GetAllPumpUseCase } from "./GetAllPumpUseCase";
+
+class GetAllPumpController {
+    async handle(req: Request, res: Response, next: NextFunction){
+        const getAllPumpController = container.resolve(GetAllPumpUseCase);
+        try {
+            const allPumpController = await getAllPumpController.execute();
+
+            res.status(201).send(allPumpController);
+
+        }catch (err){
+            messageErrorTryAction(
+                err,
+                false,
+                GetAllPumpController.name,
+                'Get All Pumps'
+            );
+            next();
+        }
+    }
+
+}
+export { GetAllPumpController }
