73aacb28aeaf464889525a48632a5629804f5727
IgorMendesPereira <igor_mendes_pereira@outlook.com>
2022-05-05 18:07:50 -0300

    Get All Scheduling Angle Ready

--- packages/backend/src/routes/schedulingangle.ts
+++ packages/backend/src/routes/schedulingangle.ts
@@ -2,9 +2,9 @@ import express from 'express';
 import authMiddleware from '../middlewares/auth';
 import { CreateSchedulingAngleController } from '../useCases/SchedulingAngle/CreateSchedulingAngle/CreateSchedulingAngleController'
 import { DeleteSchedulingAngleController } from '../useCases/SchedulingAngle/DeleteSchedulingAngle/DeleteSchedulingAngleController';
+import { GetAllSchedulingAngleController } from '../useCases/SchedulingAngle/GetAllSchedulingAngle/GetAllSchedulingAngleController';
 // import { UpdateSchedulingController } from '../useCases/SchedulingAngle/UpdateSchedulingAngle/UpdateSchedulingAngleController';
 // import { GetSchedulingAngleController } from '../useCases/SchedulingAngle/GetSchedulingAngle/GetSchedulingAngleController';
-// import { GetAllSchedulingAngleController } from '../useCases/SchedulingAngle/GetAllSchedulingAngle/GetAllSchedulingAngleController';
 
 const router = express.Router();
 
@@ -12,12 +12,12 @@ const createSchedulingAngleController = new CreateSchedulingAngleController();
 const deleteSchedulingAngleController = new DeleteSchedulingAngleController();
 // const updateSchedulingAngleController = new UpdateSchedulingAngleController();
 // const getSchedulingAngleController = new GetSchedulingAngleController();
-// const getAllSchedulingAngleController = new GetAllSchedulingAngleController();
+const getAllSchedulingAngleController = new GetAllSchedulingAngleController();
 
 router.post('/addSchedulingAngle', authMiddleware(), createSchedulingAngleController.handle);
 router.delete('/deleteSchedulingAngle/:id', authMiddleware(), deleteSchedulingAngleController.handle)
 // router.put('/updateSchedulingAngle/', authMiddleware(), updateSchedulingAngleController.handle)
 // router.get('/getSchedulingAngle/:id', authMiddleware(), getSchedulingAngleController.handle)
-// router.get('/getAllSchedulingAngle', authMiddleware(), getAllSchedulingAngleController.handle)
+router.get('/getAllSchedulingAngle', authMiddleware(), getAllSchedulingAngleController.handle)
 
 export default router;
